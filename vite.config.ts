96c70ed2839d91aec15278eba31012dba7aa4dcd
MotoIluyha <motoilyuha@mail.ru>
2025-09-05 13:29:49 +0300

    Добавлены новые зависимости в package.json и настроен прокси-сервер в vite.config.ts для API New York Times.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/nyt': {
+        target: 'https://api.nytimes.com/svc/archive/v1',
+        changeOrigin: true,
+        rewrite: p => p.replace(/^\/nyt/, '')
+      }
+    }
+  }
 })
